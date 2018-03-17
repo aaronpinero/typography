2063c936b345f24720165fb253ed74d9dd43c6ba
Aaron Pinero <aaronpinero@gmail.com>
2018-03-17 14:05:56 -0400

    added list-flex and associated classes; updated documentation and demo

--- dist/script/docs-demo.js
+++ dist/script/docs-demo.js
@@ -13,30 +13,30 @@
   
   $("#list-class-icons").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-nobullet, #list-class-indented").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-nobullet").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-nobullet, #list-class-indented").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-nobullet").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
   $("#list-class-cols2").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
   $("#list-class-cols3").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-cols2, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-cols2, #list-class-grid2, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-cols2, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-cols2, #list-class-grid2, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
@@ -44,48 +44,78 @@
   $("#list-class-nobullet").change(function(){
     if ($(this).is(':checked')) {
       $("#list-class-icons").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-indented").removeAttr('disabled').parent().removeClass('disabled');
     }
     else {
       $("#list-class-icons").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-indented").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+    }
+    SetClasses();
+  });
+
+  $("#list-class-flexcenter").change(function(){
+    if ($(this).is(':checked')) {
+      $("#list-class-flexright").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+    }
+    else {
+      $("#list-class-flexright").removeAttr('disabled').parent().removeClass('disabled');
+    }
+    SetClasses();
+  });
+
+  $("#list-class-flexright").change(function(){
+    if ($(this).is(':checked')) {
+      $("#list-class-flexcenter").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+    }
+    else {
+      $("#list-class-flexcenter").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
   $("#list-class-grid2").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid3, #list-class-grid4").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid3, #list-class-grid4").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid3, #list-class-grid4, #list-class-flex").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
   $("#list-class-grid3").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid4").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid4, #list-class-flex").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid4").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid4, #list-class-flex").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
   $("#list-class-grid4").change(function(){
     if ($(this).is(':checked')) {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-flex").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
     }
     else {
-      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-flex").removeAttr('disabled').parent().removeClass('disabled');
     }
     SetClasses();
   });
 
-  $("#list-class-indented").change(function(){
+  $("#list-class-flex").change(function(){
+    if ($(this).is(':checked')) {
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+      $("#list-class-flexcenter, #list-class-flexright").removeAttr('disabled').parent().removeClass('disabled');
+    }
+    else {
+      $("#list-class-cols2, #list-class-cols3, #list-class-grid2, #list-class-grid3, #list-class-grid4").removeAttr('disabled').parent().removeClass('disabled');
+      $("#list-class-flexcenter, #list-class-flexright").removeAttr('checked').attr('disabled','disabled').parent().addClass('disabled');
+    }
     SetClasses();
   });
 
-  $("#list-class-marginsoff").change(function(){
+  $("#list-class-indented, #list-class-marginsoff").change(function(){
     SetClasses();
   });
   
@@ -96,7 +126,7 @@
       classes += " " + $(this).attr('value');
     });
     demo.attr('class',classes);
-    if (classes.indexOf('grid') != -1 || classes.indexOf('cols') != -1) {
+    if (classes.indexOf('grid') != -1 || classes.indexOf('cols') != -1 || classes.indexOf('flex') != -1) {
       demo.find('ul, ol').hide();
     }
     else {
