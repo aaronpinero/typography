e4acaa000a15baf78c2d754daccac27b8c7cd58d
Aaron Pinero <aaronpinero@gmail.com>
2019-01-26 19:37:37 -0500

    improvement to responsive table styles and script

--- dist/script/tables.js
+++ dist/script/tables.js
@@ -1,13 +1,9 @@
 (function($) {
-
-  $('table.responsive th').each(function(i){
-    parent_table = $(this).parents('table').eq(0);
-    if ($(this).next().is('td')) {
-      $(this).nextAll('td').prepend('<span class="td-label">'+$(this).text()+'</span>');
-    }
-    else if ($(this).next().is('th') || $(this).prev().is('th')) {
-      parent_table.find('tbody tr td:nth-child('+(i+1)+')').prepend('<span class="td-label">'+$(this).text()+'</span>');
-    }
+  
+  $('table.responsive').each(function(i){
+    $(this).find('thead th').each(function(j){
+      $(this).parents('table').eq(0).find('tbody tr td:nth-child('+(j+1)+')').prepend('<span class="td-label">'+$(this).text()+'</span>');
+    });
   });
 
 })(jQuery);
