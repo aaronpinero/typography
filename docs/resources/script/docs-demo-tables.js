4214b3fc62edce68641f262c570986fc208d00f7
Aaron Pinero <aaronpinero@gmail.com>
2019-02-10 13:09:19 -0500

    tyfy-30: improvements to tables demo

--- docs/resources/script/docs-demo-tables.js
+++ docs/resources/script/docs-demo-tables.js
@@ -4,37 +4,53 @@
   
   $('#table-headers-cols').change(function(){
     if ($(this).is(':checked')) {
-      demo.find('thead').removeAttr('style');
+      if (demo.find('thead').length === 0) {
+        demo.prepend(window.demo_head);
+      }
+      else {
+        demo.find('thead').removeAttr('style');
+      }
     }
     else {
-      demo.find('thead').hide();
+      window.demo_head = demo.find('thead');
+      demo.find('thead').remove();
+    }
+    $('#table-headers-rows').trigger('change');
+    if (demo.hasClass('tyfy-responsive-processed')) {
+      TYFY_ResponsiveTablesOff();
+      TYFY_ResponsiveTablesOn();
     }
   });
   
   $('#table-headers-rows').change(function(){
     if ($(this).is(':checked')) {
-      demo.find('tr > *:first-child').removeAttr('style');
+      demo.find('tr > *:first-child').removeClass('row-hidden');
     }
     else {
-      demo.find('tr > *:first-child').hide();
+      demo.find('tr > *:first-child').addClass('row-hidden');
     }
   });
 
   $('[name="table-class"]').change(function(){
+    if ($(this).attr('value') == 'responsive') {
+      if (!$(this).is(':checked')) {
+        TYFY_ResponsiveTablesOff();
+      }
+    }
     SetClasses();
     // handle responsive tables
     if ($(this).attr('value') == 'responsive') {
       if ($(this).is(':checked')) {
-        ResponsiveTables();
-      }
-      else {
-        $('.td-label').remove();
+        TYFY_ResponsiveTablesOn();
       }
     }
   });
   
   function SetClasses() {
     classes = baseclass;
+    if (demo.hasClass('tyfy-responsive-processed')) {
+      classes += " tyfy-responsive-processed";
+    }
     $('#tables-demo input[type="checkbox"][name="table-class"]:checked').each(function(){
       classes += " " + $(this).attr('value');
     });
