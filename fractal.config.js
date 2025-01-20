577a78cce98768148b161de2657ab39ab60a7f85
Aaron Pinero <aaronpinero@gmail.com>
2025-01-20 17:39:47 -0500

    starting work on new components for documentation

--- fractal.config.js
+++ fractal.config.js
@@ -3,11 +3,11 @@
 const fractal = module.exports = require('@frctl/fractal').create();
 
 fractal.set('project.title', 'Tyfy Documentation Pattern Library');
-fractal.set('project.version', 'v2.0');
+fractal.set('project.version', 'v3.0');
 fractal.set('project.author', 'Aaron Pinero');
 
-fractal.components.set('path', __dirname + '/docs/resources/scss/components');
+fractal.components.set('path', __dirname + '/new/docs/resources/components');
 fractal.components.set('default.preview', '@preview');
 
-fractal.docs.set('path', __dirname + '/docs/resources/scss/guide');
-fractal.web.set('static.path', __dirname + '/docs/resources');
+fractal.docs.set('path', __dirname + '/new/docs/resources/guide');
+fractal.web.set('static.path', __dirname + '/new');
---
+++ new/docs/resources/guide/page-structure.md
@@ -0,0 +1,23 @@
+---
+title: Page Structure
+---
+
+The HTML structure of a documentation page looks like this:
+
+```html
+<html>
+  <body>
+    <header class="pagetitle">
+      <h1></h1>
+    </header>
+    <main class="block">
+      <nav class="nav-inpage">
+      </nav>
+      <section class="section-body">
+      </section>
+    </main>
+    <footer class="footer">
+    </footer>
+  </body>
+</html>
+```
